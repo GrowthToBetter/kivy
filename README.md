49b3b1aeb813b6f0e5fe145f8386a39ea217cdd3
baratrahjaga <baratrahjaga@gmail.com>
2023-11-21 15:00:00 +0700

    i make 2 file in train_kivy are named input-button and input-boxes, that have a function to save name of the people who launch the code and show it into a middle of the GUI

---
+++ README.md
@@ -0,0 +1,1 @@
+this is my first repository , i think that i will used to show my project, and i hope that i can get scholarship with all project that i create
---
+++ train_kivy/input-boxes1.py
@@ -0,0 +1,24 @@
+import kivy
+kivy.require('2.1.0')
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.lang import Builder
+Builder.load_file('input-button1.kv')
+
+
+class floatlayoutm(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    def press(self):
+        self.add_widget(Label(text=f'your name is {self.ids.name.text}'))
+
+class app(App):
+    def build(self):
+        return floatlayoutm()\
+        
+if __name__=='__main__':
+    app().run()

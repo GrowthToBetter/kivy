4ec14d53b751d217b622c39dbe41445bb0c99eae
baratrahjaga <baratrahjaga@gmail.com>
2023-11-22 19:51:30 +0700

    menambahkan file optimize-size, menggunakan sistem pixel dan tidak persen, namun memiliki kekurangan sizenya tidak responsible, namun penggunaan pixel membuat peletakan posisinya menjadi akurat

---
+++ train_kivy/optimize-size.py
@@ -0,0 +1,32 @@
+import kivy
+kivy.require('2.1.0')
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.lang import Builder
+from kivy.app import App
+from kivy.uix.textinput import TextInput
+
+
+Builder.load_file('optimize-size.kv')
+class main(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def submit(self):
+        self.name= self.ids.input.text
+        self.add_widget(Label(
+            text=f"your name is {self.name}",
+            size_hint=(None,None),
+            width=100,
+            height=25,
+            pos_hint={'x':0.35,'y':0.25}
+            ))
+        
+class app(App):
+    def build(self):
+        return main()
+    
+if __name__ == '__main__':
+    app().run()

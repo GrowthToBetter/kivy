b8b18fe44dd8f8d6b9f7b0b52ffd0eacb688d3b8
baratrahjaga <baratrahjaga@gmail.com>
2023-12-28 12:03:52 +0700

    menambahkan tabbedpanel dan tabbedpanelitem di file split.py agar gambar yang dipilih dapat di split di samping, dan jika memilih gambar lain akan ditampilkan di tab baru saat menekan button

--- train_kivy/split.py
+++ train_kivy/split.py
@@ -7,22 +7,33 @@ from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.lang import Builder 
 from kivy.app import App
-import copy
+from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem
+
 class main(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
+        self.tab=TabbedPanel(do_default_tab=False)
+        self.sp=Splitter(sizable_from='left')
+        self.sp.add_widget(self.tab)
+        self.count=0
+        self.pic=[]
     def press(self, target ):
-        sp=Splitter(sizable_from='left')
-        box=BoxLayout(orientation='horizontal', size=(self.width,self.height))
+        self.pic.append(f'{target}')
+        # box=BoxLayout(orientation='horizontal', size=(self.width,self.height))
         image= Image(opacity=0.5,source='')
         try:
             image.source=f'{target[0]}'
         except:
             pass
-        box.add_widget(image)
-        sp.add_widget(box)
-        self.ids.wrap.add_widget(sp)
-
+        # box.add_widget(image)
+        # sp.add_widget(box)
+        for i in range(len(self.pic)):
+            item=TabbedPanelItem(text=f'tab {i}')
+            item.add_widget(image)
+        self.tab.add_widget(item)
+        if self.count==0:
+            self.ids.wrap.add_widget(self.sp)
+            self.count=1
 class app(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)

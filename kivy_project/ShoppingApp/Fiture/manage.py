b6e2d66db7e447dd0ca5a26e470b0f4c3ca8f3fc
baratrahjaga <baratrahjaga@gmail.com>
2024-03-19 17:51:58 +0700

    membuat manage.py untuk memanagement file feature yang ada lalu membuat optimasi pada kode login.py, memperbaiki bug sebelumnya dimana screen manager dan file .py pada fiture bertabrakan

---
+++ kivy_project/ShoppingApp/Fiture/manage.py
@@ -0,0 +1,20 @@
+import kivy
+kivy.require('2.2.1')
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivymd.uix.screen import MDScreen
+from kivy.uix.screenmanager import ScreenManager
+import os, sys
+from kivy.core.window import Window
+
+class Manage(MDScreen):
+    def __init__(self, **kwargs):
+        self.sm= ScreenManager()
+        self.sm.add_widget(Builder.load_file(self.Resource_path('Fiture/Login.kv')))
+        self.sm.add_widget(Builder.load_file(self.Resource_path('Fiture/list_item.kv')))
+    def Resource_path(self,Relative_path):
+        try:
+            Base_path=sys._MEIPASS
+        except Exception:
+            Base_path=os.path.abspath('kivy_project/ShoppingApp/')
+        return os.path.join(Base_path, Relative_path)
--- kivy_project/ShoppingApp/main.py
+++ kivy_project/ShoppingApp/main.py
@@ -1,16 +1,23 @@
-from kivymd.app import MDApp, ThemeManager
-from kivy.uix.screenmanager import ScreenManager
-from kivy.lang import Builder
-from Fiture import list_item
+from kivymd.app import MDApp
+from Fiture.manage import Manage
+import os, sys
+from kivy.resources import resource_add_path
+
+
+class Load(Manage):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
 class MyApp(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.sm=ScreenManager()
         self.theme_cls.primary_palette='LightBlue'
- 
 
     def build(self):
-        return self.sm
+        return Load().sm
 
 if __name__=="__main__":
+    if hasattr(sys, "_MEIPASS"):
+        resource_add_path((os.path.join(sys._MEIPASS)))
+
     MyApp().run()

2308f7919db704d4beaefb1a1ae145b2db97b39e
baratrahjaga <baratrahjaga@gmail.com>
2024-04-18 14:17:18 +0700

    menambahkan list bottom sheet pada setiap item image

--- kivy_project/ShoppingApp/Fiture/manage.py
+++ kivy_project/ShoppingApp/Fiture/manage.py
@@ -7,14 +7,8 @@ from kivy.uix.screenmanager import ScreenManager
 import os, sys
 from kivy.core.window import Window
 from kivy.uix.label import Label
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.expansionpanel import (
-    MDExpansionPanel,
-    MDExpansionPanelThreeLine,
-)
-class Content(MDBoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+from kivymd.toast import toast
+from kivymd.uix.bottomsheet import MDListBottomSheet
 
 
 class Manage(MDScreen):
@@ -27,5 +21,16 @@ class Manage(MDScreen):
         except Exception:
             Base_path=os.path.abspath('kivy_project/ShoppingApp/')
         return os.path.join(Base_path, Relative_path)
-    def press(self, definition):
-        print(f'{definition}')
+    
+    def callback_for_menu_items(self, list_item_text):
+        toast(list_item_text)
+
+    def show_price(self):
+        bottom_sheet_menu = MDListBottomSheet()
+        bottom_sheet_menu.add_item(
+            f"Buy Now",
+            lambda x: self.callback_for_menu_items(
+                f"Item Purchase"
+            ),
+        )
+        bottom_sheet_menu.open()
